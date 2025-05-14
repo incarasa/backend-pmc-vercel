5b4f5da104cb73e938059f1d1a6bf6b585918575
Raul Insuasty <r.insuasty@uniandes.edu.co>
2025-05-13 21:37:06 -0500

    vercel config

---
+++ api/index.js
@@ -0,0 +1,4 @@
+const serverless = require("serverless-http");
+const app = require("../index"); // importa tu app de Express
+
+module.exports = serverless(app);
--- index.js
+++ index.js
@@ -26,6 +26,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server and listen on port 3000
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+module.exports = app;
